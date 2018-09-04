ccdf3b42031437816d63f14fc6bc06562b5270d9
Maciej Wiczk <mezazel@gmail.com>
2018-09-04 20:13:19 +0200

    separating processpollexecutor and called function

--- exporter.py
+++ exporter.py
@@ -8,30 +8,21 @@ input_path = r'C:\Users\Maciek\PycharmProjects\PWX_2_TCX_Parser\pwx'
 output_path = r'C:\Users\Maciek\PycharmProjects\PWX_2_TCX_Parser\tcx'
 
 
-def export_tcx_file(pwx_path, tcx_path):
-    def convert_pwx_2_tcx(file):
-        pwx = open_pwx_and_fix_tags(os.path.join(pwx_path, file + '.pwx'))
-        start_time = str(get_start_time(pwx))
-        workout_data = get_workout_data(pwx)
-        activity_type = get_activity_type(pwx)
-        total_time = get_duration(pwx)
-        total_dist = get_dist(pwx)
-        avg_hr = get_avg_heart_rate(workout_data)
-        max_hr = get_max_heart_rate(workout_data)
-        tcx = generate_tcx_content(file, activity_type, start_time, total_time, total_dist, avg_hr, max_hr,
-                                   workout_data)
-        with open(os.path.join(tcx_path, file + '.tcx'), "wb") as f:
-            f.write(tostring(tcx))
-            f.close()
-        progress_bar.next()
-
-    files = get_new_files(pwx_path, tcx_path)
-    if files.__len__():
-        progress_bar = IncrementalBar('Processing', max=files.__len__(), suffix='%(percent).1f%% - %(eta)ds')
-        with Executor(max_workers=None) as exe:
-            jobs = [exe.submit(convert_pwx_2_tcx(file)) for file in files]
-    else:
-        print("No new Files found.")        
+def convert_pwx_2_tcx(pwx_path, tcx_path, file):
+    pwx = open_pwx_and_fix_tags(os.path.join(pwx_path, file + '.pwx'))
+    start_time = str(get_start_time(pwx))
+    workout_data = get_workout_data(pwx)
+    activity_type = get_activity_type(pwx)
+    total_time = get_duration(pwx)
+    total_dist = get_dist(pwx)
+    avg_hr = get_avg_heart_rate(workout_data)
+    max_hr = get_max_heart_rate(workout_data)
+    tcx = generate_tcx_content(file, activity_type, start_time, total_time, total_dist, avg_hr, max_hr,
+                               workout_data)
+    with open(os.path.join(tcx_path, file + '.tcx'), "wb") as f:
+        f.write(tostring(tcx))
+        f.close()
+    progress_bar.next()
 
 
 def get_new_files(pwx_path, tcx_path):
@@ -45,4 +36,11 @@ def get_file_list(path, extension):
 
 
 if __name__ == '__main__':
-    export_tcx_file(input_path, output_path)
+    files = get_new_files(input_path, output_path)
+    if files.__len__():
+        progress_bar = IncrementalBar('Processing', max=files.__len__(), suffix='%(percent).1f%% - %(eta)ds')
+        with Executor(max_workers=8) as exe:
+            for file in files:
+                exe.submit(convert_pwx_2_tcx(input_path, output_path, file))
+    else:
+        print("No new Files found.")
