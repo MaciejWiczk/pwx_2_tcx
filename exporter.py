f542949d2639f8034c41ad5fd7ef320aa2ebc484
Maciej Wiczk <mezazel@gmail.com>
2018-09-05 17:29:39 +0200

    Final changes to concurrecy. Like a charm after Łukasz fix

--- exporter.py
+++ exporter.py
@@ -1,8 +1,9 @@
 from pwx_importer import *
 from tcx_creator import *
 from progress.bar import IncrementalBar
-from concurrent.futures import ProcessPoolExecutor as Executor
+import concurrent.futures
 import os
+from pathlib import Path
 
 input_path = r'C:\Users\Maciek\PycharmProjects\PWX_2_TCX_Parser\pwx'
 output_path = r'C:\Users\Maciek\PycharmProjects\PWX_2_TCX_Parser\tcx'
@@ -19,10 +20,10 @@ def convert_pwx_2_tcx(pwx_path, tcx_path, file):
     max_hr = get_max_heart_rate(workout_data)
     tcx = generate_tcx_content(file, activity_type, start_time, total_time, total_dist, avg_hr, max_hr,
                                workout_data)
-    with open(os.path.join(tcx_path, file + '.tcx'), "wb") as f:
+    p = Path(tcx_path)
+    with open(str(p / f'{file}.tcx'), "wb") as f:
         f.write(tostring(tcx))
         f.close()
-    progress_bar.next()
 
 
 def get_new_files(pwx_path, tcx_path):
@@ -35,12 +36,18 @@ def get_file_list(path, extension):
     return {str.replace(file, extension, '') for file in os.listdir(path) if os.path.isfile(os.path.join(path, file))}
 
 
-if __name__ == '__main__':
+def main():
+    Path(output_path).mkdir(parents=True, exist_ok=True)
     files = get_new_files(input_path, output_path)
-    if files.__len__():
-        progress_bar = IncrementalBar('Processing', max=files.__len__(), suffix='%(percent).1f%% - %(eta)ds')
-        with Executor(max_workers=8) as exe:
-            for file in files:
-                exe.submit(convert_pwx_2_tcx(input_path, output_path, file))
+    if files:
+        progress_bar = IncrementalBar('Processing', max=len(files), suffix='%(percent).1f%% - %(eta)ds, elapsed: %(elapsed)ds')
+        with concurrent.futures.ProcessPoolExecutor() as exe:
+            futures = {exe.submit(convert_pwx_2_tcx, input_path, output_path, file): file for file in files}
+            for future in concurrent.futures.as_completed(futures):
+                progress_bar.next()
     else:
         print("No new Files found.")
+
+
+if __name__ == '__main__':
+    main()
